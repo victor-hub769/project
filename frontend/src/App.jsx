3da06d4e5687c5722da998491fbce061b5d19048
victor-hub769 <victormuriithi996@gmail.com>
2023-11-30 12:49:02 +0300

    Frontend creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,25 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <div>MY DEVELOPER PORTFOLIO</div>
+      <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> 
+        <Navbar/>
+        <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+
+
+      </div>
+      </BrowserRouter>
     </>
   );
 };
