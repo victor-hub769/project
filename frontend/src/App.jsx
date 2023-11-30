6b1461d3999dd70552d9dc675066be5c4fa439ed
victor-hub769 <victormuriithi996@gmail.com>
2023-11-30 11:27:34 +0300

    Frontend creation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>MY DEVELOPER PORTFOLIO</div>
+    </>
+  );
+};
+
+export default App;
